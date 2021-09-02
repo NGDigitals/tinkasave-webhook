2a2e6d478ae12f7f9e033c353ee7baa2d786cede
NG Digitals <ngdigitalsng@gmail.com>
2021-09-02 19:32:42 +0100

    New Commit

--- app.js
+++ app.js
@@ -17,76 +17,78 @@ app.post("/webhook/paystack", async (req, res) => {
                 const response = await db.getTransactionByReference(reference);
                 if(response.rows){
                     const transaction = response.rows[0];
-                    if(transaction.buddie_id !== null){
-                        const target = 'buddie';
-                        const buddieID = transaction.buddie_id;
-                        let savings = await db.getTotalSavings(target, buddieID);
-                        let withdrawal = await db.getTotalWithdrawal(target, buddieID);
-                        let interest = await db.getTotalInterest(target, buddieID);
-                        let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
-                        let balance = (totalSavings + (interest.rows[0].sum !== null ? 
-                                interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
-                        console.log('All: ', buddieID, totalSavings, savings.rows[0].sum, withdrawal.rows[0].sum, interest.rows[0].sum, balance)
-                        await db.beginTransaction();
-                        console.log('Testing...1');
-                        await db.updateSaving(target, buddieID, totalSavings, balance);
-                        console.log('Testing...2');
-                        await db.updateTransaction(transaction.id, 'Completed');
-                        console.log('Testing...3');
-                        await db.commitTransaction();
-                        console.log('Testing...4');
-                        res.sendStatus(200);
-                    }else if(transaction.group_id !== null){
-                        let target = 'member';
-                        const memberID = transaction.member_id;
-                        let savings = await db.getTotalSavings(target, memberID);
-                        let withdrawal = await db.getTotalWithdrawal(target, memberID);
-                        let interest = await db.getTotalInterest(target, memberID);
-                        let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
-                        let balance = (totalSavings + (interest.rows[0].sum !== null ? 
-                                interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
-                        // await db.beginTransaction();
-                        // await db.updateSaving(target, memberID, totalSavings, balance);
-                        target = 'group';
-                        const groupID = transaction.group_id;
-                        savings = await db.getTotalSavings(target, groupID);
-                        withdrawal = await db.getTotalWithdrawal(target, groupID);
-                        interest = await db.getTotalInterest(target, groupID);
-                        totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
-                        balance = (totalSavings + (interest.rows[0].sum !== null ? 
-                                interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
-                        // await db.updateSaving(target, groupID, totalSavings, balance);
-                        await db.updateTransaction(transaction.id, 'Completed');
-                        // await db.commitTransaction();
-                        res.sendStatus(200);
-                    }else if(transaction.smooth_id !== null){
-                        const target = 'smooth';
-                        const smoothID = transaction.smooth_id;
-                        let savings = await db.getTotalSavings(target, smoothID);
-                        let withdrawal = await db.getTotalWithdrawal(target, smoothID);
-                        let interest = await db.getTotalInterest(target, smoothID);
-                        let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
-                        let balance = (totalSavings + (interest.rows[0].sum !== null ? 
-                                interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
-                        await db.beginTransaction();      
-                        // await db.updateSaving(target, smoothID, totalSavings, balance);
-                        await db.updateTransaction(transaction.id, 'Completed');
-                        // await db.commitTransaction();
-                        res.sendStatus(200);
-                    }else if(transaction.kid_id !== null){
-                        const target = 'kid';
-                        const kidID = transaction.kid_id;
-                        let savings = await db.getTotalSavings(target, kidID);
-                        let withdrawal = await db.getTotalWithdrawal(target, kidID);
-                        let interest = await db.getTotalInterest(target, kidID);
-                        let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
-                        let balance = (totalSavings + (interest.rows[0].sum !== null ? 
-                                interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
-                        // await db.beginTransaction();
-                        // await db.updateSaving(target, kidID, totalSavings, balance);
-                        await db.updateTransaction(transaction.id, 'Completed');
-                        // await db.commitTransaction();
-                        res.sendStatus(200);
+                    if(transaction !== undefined){
+                        if(transaction.buddie_id !== null){
+                            const target = 'buddie';
+                            const buddieID = transaction.buddie_id;
+                            let savings = await db.getTotalSavings(target, buddieID);
+                            let withdrawal = await db.getTotalWithdrawal(target, buddieID);
+                            let interest = await db.getTotalInterest(target, buddieID);
+                            let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
+                            let balance = (totalSavings + (interest.rows[0].sum !== null ? 
+                                    interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
+                            console.log('Testing: ', buddieID, totalSavings, savings.rows[0].sum, withdrawal.rows[0].sum, interest.rows[0].sum, balance)
+                            await db.beginTransaction();
+                            console.log('Testing...1');
+                            await db.updateSaving(target, buddieID, totalSavings, balance);
+                            console.log('Testing...2');
+                            await db.updateTransaction(transaction.id, 'Completed');
+                            console.log('Testing...3');
+                            await db.commitTransaction();
+                            console.log('Testing...4');
+                            res.sendStatus(200);
+                        }else if(transaction.group_id !== null){
+                            let target = 'member';
+                            const memberID = transaction.member_id;
+                            let savings = await db.getTotalSavings(target, memberID);
+                            let withdrawal = await db.getTotalWithdrawal(target, memberID);
+                            let interest = await db.getTotalInterest(target, memberID);
+                            let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
+                            let balance = (totalSavings + (interest.rows[0].sum !== null ? 
+                                    interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
+                            // await db.beginTransaction();
+                            // await db.updateSaving(target, memberID, totalSavings, balance);
+                            target = 'group';
+                            const groupID = transaction.group_id;
+                            savings = await db.getTotalSavings(target, groupID);
+                            withdrawal = await db.getTotalWithdrawal(target, groupID);
+                            interest = await db.getTotalInterest(target, groupID);
+                            totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
+                            balance = (totalSavings + (interest.rows[0].sum !== null ? 
+                                    interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
+                            // await db.updateSaving(target, groupID, totalSavings, balance);
+                            await db.updateTransaction(transaction.id, 'Completed');
+                            // await db.commitTransaction();
+                            res.sendStatus(200);
+                        }else if(transaction.smooth_id !== null){
+                            const target = 'smooth';
+                            const smoothID = transaction.smooth_id;
+                            let savings = await db.getTotalSavings(target, smoothID);
+                            let withdrawal = await db.getTotalWithdrawal(target, smoothID);
+                            let interest = await db.getTotalInterest(target, smoothID);
+                            let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
+                            let balance = (totalSavings + (interest.rows[0].sum !== null ? 
+                                    interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
+                            await db.beginTransaction();      
+                            // await db.updateSaving(target, smoothID, totalSavings, balance);
+                            await db.updateTransaction(transaction.id, 'Completed');
+                            // await db.commitTransaction();
+                            res.sendStatus(200);
+                        }else if(transaction.kid_id !== null){
+                            const target = 'kid';
+                            const kidID = transaction.kid_id;
+                            let savings = await db.getTotalSavings(target, kidID);
+                            let withdrawal = await db.getTotalWithdrawal(target, kidID);
+                            let interest = await db.getTotalInterest(target, kidID);
+                            let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
+                            let balance = (totalSavings + (interest.rows[0].sum !== null ? 
+                                    interest.rows[0].sum : 0)) - (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
+                            // await db.beginTransaction();
+                            // await db.updateSaving(target, kidID, totalSavings, balance);
+                            await db.updateTransaction(transaction.id, 'Completed');
+                            // await db.commitTransaction();
+                            res.sendStatus(200);
+                        }
                     }
                 }
             }catch(error){
