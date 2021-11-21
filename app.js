a33d3df05b1dd3acf80e119906953d78dbb51375
NG Digitals <ngdigitalsng@gmail.com>
2021-11-21 17:42:45 +0100

    New Commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var crypto = require('crypto');
-var secret = 'sk_test_104e2d6712930262f6f9b61b824459b84a2268b6';//process.env.SECRET_KEY;
+var secret = 'sk_live_6f302e970dd45ae4164a338c3867986f3571203e';//process.env.SECRET_KEY;
 const db = require('./connection');
 const express = require('express')
 const bodyParser = require('body-parser');
@@ -9,44 +9,33 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.post("/webhook/paystack", async (req, res) => {
     const json = req.body;
-    console.log(`JSON Received ${json}.`)
-    var hash = crypto.createHmac('sha512', secret).update(JSON.stringify(json)).digest('hex');
-    if (hash == req.headers['x-paystack-signature']) {
-        if(json.event == 'charge.success'){
-            try{
-                const reference = json.data.reference;
-                console.log(`Data Ref ${reference}.`)
-                const response = await db.getTransactionByReference(reference);
-                console.log(`Data Response ${response}.`)
-                if(response.rows){
-                    const transaction = response.rows[0];
-                    console.log(`Data Trans ${transaction}.`)
-                    if(transaction !== undefined){
-                        console.log(`Updating Trans...`)
+    // var hash = crypto.createHmac('sha512', secret).update(JSON.stringify(json)).digest('hex');
+    // if (hash == req.headers['x-paystack-signature']) {
+        try{
+            const reference = json.data.reference;
+            const response = await db.getTransactionByReference(reference);
+            if(response.rows){
+                const transaction = response.rows[0];
+                if(transaction !== undefined){
+                    if(json.event == 'charge.success'){
                         if(transaction.buddie_id !== null /*&& transaction.status !== 'Completed'*/){
-                            console.log(`Updating Buddie...1`)
                             const target = 'buddie';
                             const buddieID = transaction.buddie_id;
                             let savings = await db.getTotalSavings(target, buddieID);
                             let withdrawal = await db.getTotalWithdrawal(target, buddieID);
-                            console.log(`Updating Buddie...2`)
                             // let interest = await db.getTotalInterest(target, buddieID);
                             let totalSavings = (savings.rows[0].sum !== null ? savings.rows[0].sum : 0) + transaction.amount;
-                            console.log(`Updating Buddie...3`)
                             // let totalWithdrawal = 
                             //     withdrawal.rows[0].sum != null ? 
                             //     (withdrawal.rows[0].sum < 0 ? withdrawal.rows[0].sum : -withdrawal.rows[0].sum) : 0
                             // let balance = totalSavings + totalWithdrawal;
                             let balance = totalSavings + /*(interest.rows[0].sum !== null ? 
                                 interest.rows[0].sum : 0)) +*/ (withdrawal.rows[0].sum != null ? withdrawal.rows[0].sum : 0)
-                                console.log(`Updating Buddie...3`)
                             await db.beginTransaction();
                             await db.updateSaving(target, buddieID, totalSavings, balance);
-                            console.log(`Updating Buddie...4`)
                             await db.updateTransaction(transaction.id, 'Completed');
                             await db.commitTransaction();
-                            console.log(`Updating Buddie...5`)
-                        }else if(transaction.group_id !== null && transaction.status !== 'Completed'){
+                        }else if(transaction.group_id !== null /*&& transaction.status !== 'Completed'*/){
                             let target = 'member';
                             const memberID = transaction.member_id;
                             let savings = await db.getTotalSavings(target, memberID);
@@ -93,7 +82,7 @@ app.post("/webhook/paystack", async (req, res) => {
                             await db.updateSaving(target, smoothID, totalSavings, balance);
                             await db.updateTransaction(transaction.id, 'Completed');
                             await db.commitTransaction();
-                        }else if(transaction.kid_id !== null && transaction.status !== 'Completed'){
+                        }else if(transaction.kid_id !== null /*&& transaction.status !== 'Completed'*/){
                             const target = 'kid';
                             const kidID = transaction.kid_id;
                             let savings = await db.getTotalSavings(target, kidID);
@@ -111,21 +100,22 @@ app.post("/webhook/paystack", async (req, res) => {
                             await db.updateTransaction(transaction.id, 'Completed');
                             await db.commitTransaction();
                         }
-                        res.sendStatus(200);
-                    }else
-                        res.sendStatus(404);
+                    }else if(json.event == 'charge.failed'){
+                        await db.updateTransaction(transaction.id, 'Failed');
+                    }
+                    res.sendStatus(200);
                 }else
                     res.sendStatus(404);
-            }catch(error){
-                console.log(`Database Error ${error}.`)
-                await db.rollbackTransaction();
-                // await db.release();
+            }else
                 res.sendStatus(404);
-            }
-        }else
+        }catch(error){
+            console.log(`Database Error ${error}.`)
+            await db.rollbackTransaction();
+            // await db.release();
             res.sendStatus(404);
-    }else
-        res.sendStatus(401);
+        }
+    // }else
+    //     res.sendStatus(401);
 });
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
